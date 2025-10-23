dc00eaf02ad899a3f487dc7aa553bdba6e7ea7db
David <valkeshar@gmail.com>
2025-10-23 15:12:29 +0200

    Added small fixes to the text in the script

--- run_game.sh
+++ run_game.sh
@@ -3,12 +3,13 @@ echo "David Skansholms program"
 mkdir David_Skansholm_labb
 cp *.java David_Skansholm_labb/
 cd David_Skansholm_labb
-pwd
+echo "Running game from $PWD" 
 echo "Compiling game..."
 javac Guesser.java
 javac GuessingGame.java
 echo "Running game..."
 java GuessingGame
-echo "Done"
+echo "Done!"
+echo "Removing class files..."
 rm *.class
 ls
